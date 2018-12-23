e0cd795f5cc89d9a86412e44fc6c3e976a1895bd
Marczu <marczu09@gmail.com>
2018-12-23 19:45:29 +0100

    Add get all projects action and reducer

---
+++ src/reducers/projectReducer.js
@@ -0,0 +1,22 @@
+import {GET_PROJECTS} from "../actions/types";
+
+const initialState = {
+    projects: [],
+    project: {}
+};
+
+export default function (state = initialState, action) {
+
+    switch (action.type) {
+
+        case GET_PROJECTS:
+            return {
+                ...state,
+                projects: action.playload
+            };
+
+        default:
+            return state;
+    }
+
+}
