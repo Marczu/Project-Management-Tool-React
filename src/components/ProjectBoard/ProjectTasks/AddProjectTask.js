b2707ceddb76ec92d6060ab4b4f4108c168cdf30
Marczu <marczu09@gmail.com>
2019-01-25 10:48:45 +0100

    Woring sending new projectTask to databse

--- src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -13,13 +13,33 @@ class AddProjectTask extends Component {
 
       this.state = {
         "summary": "",
-        "acceptanceCriteria": "null",
+        "acceptanceCriteria": "",
         "status": "",
         "priority": 0,
         "dueDate": "",
         "projectIdentifier": id,
         "errors": {}
       }
+      this.onChange = this.onChange.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+      this.setState({[e.target.name]:e.target.value});
+  }
+
+  onSubmit(e){
+      e.preventDefault();
+      
+      const newTask = {
+        "summary": this.state.summary,
+        "acceptanceCriteria": this.state.acceptanceCriteria,
+        "status": this.state.status,
+        "priority": this.state.priority,
+        "dueDate": this.state.dueDate,
+      }
+
+      this.props.addProjectTask(this.state.projectIdentifier, newTask, this.props.history)
   }
 
   render() {
@@ -34,30 +54,38 @@ class AddProjectTask extends Component {
                     </Link>
                     <h4 className="display-4 text-center">Dodaj nowe zadanie</h4>
                     <p className="lead text-center">Nazwa Projektu + Kod Projektu</p>
-                    <form >
+                    <form onSubmit={this.onSubmit}>
                         <div className="form-group">
                             <input type="text" 
                             className="form-control form-control-lg" 
                             name="summary" 
                             placeholder="Podsumowanie zadania" 
-                            value={this.state.summary}/>
+                            value={this.state.summary}
+                            onChange={this.onChange}
+                            />
                         </div>
                         <div className="form-group">
                             <textarea className="form-control form-control-lg" 
                             placeholder="Kryteria przyjęcia" 
                             name="acceptanceCriteria" 
-                            value={this.state.acceptanceCriteria}></textarea>
+                            value={this.state.acceptanceCriteria}
+                            onChange={this.onChange}
+                            />
                         </div>
                         <h6>Termin zadania</h6>
                         <div className="form-group">
                             <input type="date" 
                             className="form-control form-control-lg" 
                             name="dueDate" 
-                            value={this.state.dueDate}/>
+                            value={this.state.dueDate}
+                            onChange={this.onChange}
+                            />
                         </div>
                         <div className="form-group">
                             <select className="form-control form-control-lg" name="priority"
-                            value={this.state.priority}>
+                            value={this.state.priority}
+                            onChange={this.onChange}
+                            >
                                 <option value={0}>Wybierz priorytet</option>
                                 <option value={1}>Wysoki</option>
                                 <option value={2}>Średni</option>
@@ -67,11 +95,13 @@ class AddProjectTask extends Component {
 
                         <div className="form-group">
                             <select className="form-control form-control-lg" name="status"
-                            value={this.state.status}>
+                            value={this.state.status}
+                            onChange={this.onChange}
+                            >
                                 <option value="">Wybierz status</option>
-                                <option value="TO_DO">DO ZROBIENIA</option>
-                                <option value="IN_PROGRESS">W TRAKCIE</option>
-                                <option value="DONE">ZROBIONY</option>
+                                <option value="DO ZROBIENIA">DO ZROBIENIA</option>
+                                <option value="W TRAKCIE">W TRAKCIE</option>
+                                <option value="ZROBIONY">ZROBIONY</option>
                             </select>
                         </div>
 
