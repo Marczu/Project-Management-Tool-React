e39310ed6aa4d2972dcfd0dd9c3923b49e1b16cc
Marczu <marczu09@gmail.com>
2019-01-26 11:14:15 +0100

    Woring display all project tasks in projectboard

--- src/components/ProjectBoard/Backlog.js
+++ src/components/ProjectBoard/Backlog.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom"
 import ProjectTask from './ProjectTasks/ProjectTask';
 
 
  class Backlog extends Component {
+
   render() {
+    const {project_tasks_prop} = this.props
+
+    const tasks = project_tasks_prop.map(project_task =>(
+        <ProjectTask key={project_task.id} project_task={project_task}/>
+    ))
+
     return (
         <div className="container">
             <div className="row">
@@ -14,7 +20,11 @@ import ProjectTask from './ProjectTasks/ProjectTask';
                             <h3>DO ZROBIENIA</h3>
                         </div>
                     </div>
-                  <ProjectTask/>
+                    {tasks}
+                    {
+                        //insert tasks here
+                    }
+
                 </div>
                 <div className="col-md-4">
                     <div className="card text-center mb-2">
