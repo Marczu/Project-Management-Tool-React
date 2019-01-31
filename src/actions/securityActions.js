a87aaad306ac4d2316454d7a21a6c7ef59a73d57
Marczu <marczu09@gmail.com>
2019-01-31 12:13:48 +0100

    Add login action and securityReducer  -add jwttoken decoder

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setJWTToken from '../securityUtils/setJWTToken';
+import jwt_decode from "jwt-decode"
 
 export const createNewUser = (newUser, history) => async dispatch => {
     try {
@@ -15,4 +17,32 @@ export const createNewUser = (newUser, history) => async dispatch => {
             payload: err.response.data
         })
     }
+}
+
+export const login = LoginRequest => async dispatch => {
+
+    try {
+    // Post => Login Request
+    const res = await axios.post("/api/users/login", LoginRequest);
+    //extract token from res.data
+    const {token} = res.data
+    //store tonen in localStorage
+    localStorage.setItem("jwtToken", token)
+    //set token in headers
+    setJWTToken(token)
+    //decode token in React
+    const decoded = jwt_decode(token)
+    //dispatch to our security reducer
+    dispatch({
+        type: SET_CURRENT_USER,
+        payload: decoded
+    })
+
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    }
+    
 }
